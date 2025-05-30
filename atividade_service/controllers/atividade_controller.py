51c300d686699a43dae8b8ffbbc7d156e9291992
Marcos Paulo F Garcia <marcos.garcia@aluno.faculdadeimpacta.com.br>
2025-05-29 21:27:20 -0300

    Update atividade_controller.py

--- atividade_service/controllers/atividade_controller.py
+++ atividade_service/controllers/atividade_controller.py
@@ -1,13 +1,29 @@
-from flask import Blueprint, jsonify
-from models import atividade_model
-from clients.pessoa_service_client import PessoaServiceClient
+from flask import Blueprint, jsonify, request
+from ..models import atividade_model
+from atividade_service.clients.pessoa_service_client import PessoaServiceClient
 
 atividade_bp = Blueprint('atividade_bp', __name__)
 
-@atividade_bp.route('/', methods=['GET'])
-def listar_atividades():
-    atividades = atividade_model.listar_atividades()
-    return jsonify(atividades)
+@atividade_bp.route('/', methods=['GET', 'POST'])
+def atividades():
+    if request.method == 'GET':
+        atividades = atividade_model.listar_atividades()
+        return jsonify(atividades)
+    
+    elif request.method == 'POST':
+        data = request.get_json()
+        
+        professor = PessoaServiceClient.obter_professor(data.get('professor_id'))
+        if not professor:
+            return jsonify({'erro': 'Professor não encontrado'}), 404
+            
+        nova_atividade = atividade_model.criar_atividade(
+            id_disciplina=data['id_disciplina'],
+            enunciado=data['enunciado'],
+            professor_id=data['professor_id']
+        )
+        
+        return jsonify(nova_atividade), 201
 
 @atividade_bp.route('/<int:id_atividade>', methods=['GET'])
 def obter_atividade(id_atividade):
