66b22928adfd150f84414089805c840c094b9d59
Marcos Paulo F Garcia <marcos.garcia@aluno.faculdadeimpacta.com.br>
2025-05-29 21:31:22 -0300

    Update pessoa_controller.py

--- pessoa_service/controllers/pessoa_controller.py
+++ pessoa_service/controllers/pessoa_controller.py
@@ -1,7 +1,15 @@
 from flask import Blueprint, jsonify
 from models import leciona_model
+import requests
+from services.professor_service import ProfessorService  
+
 
 pessoa_bp = Blueprint('pessoa_bp', __name__)
+
+def validar_professor(professor_id):
+    resp = requests.get(f"https://projeto-api-jdan.onrender.com/professores/{professor_id}")
+    return resp.status_code == 200
+
 
 @pessoa_bp.route('/leciona/<int:id_professor>/<int:id_disciplina>', methods=['GET'])
 def verificar_leciona(id_professor, id_disciplina):
