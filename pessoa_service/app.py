2e9e365a6eb27b3534d76220e05ec00aac1e225b
Marcos Paulo F Garcia <marcos.garcia@aluno.faculdadeimpacta.com.br>
2025-05-29 21:29:23 -0300

    Update app.py

--- pessoa_service/app.py
+++ pessoa_service/app.py
@@ -1,8 +1,16 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
+
 from config import create_app
+from atividade_service.controllers.atividade_controller import atividade_bp
 from controllers.pessoa_controller import pessoa_bp
 
 app = create_app()
+
+
+app.register_blueprint(atividade_bp, url_prefix='/atividades')
 app.register_blueprint(pessoa_bp, url_prefix='/pessoas')
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5001)
+    app.run(host='0.0.0.0', port=5001)
